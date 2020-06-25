4d688e9fea0ef06cf25655700ff6be03cd2c8a5d
David Barcenas <daveepro@outlook.com>
2020-06-25 18:39:42 -0500

    feat(slideshow): Create page view for slides

--- lib/pages/slideshow_page.dart
+++ lib/pages/slideshow_page.dart
@@ -5,7 +5,36 @@ class SlideShowPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: SvgPicture.asset('assets/svg/slide-1.svg')),
+      body: Center(child: _Slides()),
+    );
+  }
+}
+
+class _Slides extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: PageView(children: <Widget>[
+        _Slide('assets/svg/slide-1.svg'),
+        _Slide('assets/svg/slide-2.svg'),
+        _Slide('assets/svg/slide-3.svg'),
+      ]),
+    );
+  }
+}
+
+class _Slide extends StatelessWidget {
+  final String svg;
+
+  const _Slide(this.svg);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      padding: EdgeInsets.all(18),
+      child: SvgPicture.asset(svg),
     );
   }
 }

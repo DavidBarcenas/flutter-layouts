9646abb78bcfd86c89ce36d10cc7bdb2179ed042
David Barcenas <daveepro@outlook.com>
2020-07-07 18:30:04 -0500

    refactor(emergency): Use parameters in the header widget

--- lib/pages/emergency_page.dart
+++ lib/pages/emergency_page.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:layouts/widgets/header.dart';
 
 class EmergencyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: IconHeader());
+    return Scaffold(
+        body: IconHeader(
+      icon: FontAwesomeIcons.plus,
+      subtitle: 'Haz Solicitado',
+      title: 'Asistencia Médica',
+    ));
   }
 }
--- lib/widgets/header.dart
+++ lib/widgets/header.dart
@@ -2,16 +2,31 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class IconHeader extends StatelessWidget {
+  final IconData icon;
+  final String title, subtitle;
+  final Color primaryColor;
+  final Color secondaryColor;
+
+  static const pcolor = Color(0xff526bf6);
+  static const scolor = Color(0xff67acf2);
+
+  const IconHeader(
+      {@required this.icon,
+      @required this.title,
+      @required this.subtitle,
+      this.primaryColor = pcolor,
+      this.secondaryColor = scolor});
+
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: <Widget>[
-        _BackgroundHeader(),
+        _BackgroundHeader(this.primaryColor, this.secondaryColor),
         Positioned(
             top: -40,
             left: -40,
             child: FaIcon(
-              FontAwesomeIcons.plus,
+              this.icon,
               size: 200,
               color: Colors.white.withOpacity(0.2),
             )),
@@ -22,14 +37,14 @@ class IconHeader extends StatelessWidget {
               width: double.infinity,
             ),
             Text(
-              'Haz solicitado',
+              this.subtitle,
               style: TextStyle(fontSize: 20, color: Colors.white60),
             ),
             SizedBox(
               height: 20,
             ),
             Text(
-              'Asistencia Médica',
+              this.title,
               style: TextStyle(
                   fontSize: 25,
                   fontWeight: FontWeight.bold,
@@ -39,7 +54,7 @@ class IconHeader extends StatelessWidget {
               height: 20,
             ),
             FaIcon(
-              FontAwesomeIcons.plus,
+              this.icon,
               size: 45,
               color: Colors.white,
             )
@@ -51,9 +66,13 @@ class IconHeader extends StatelessWidget {
 }
 
 class _BackgroundHeader extends StatelessWidget {
-  const _BackgroundHeader({
-    Key key,
-  }) : super(key: key);
+  final Color primaryColor;
+  final Color secondaryColor;
+
+  const _BackgroundHeader(
+    this.primaryColor,
+    this.secondaryColor,
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -65,7 +84,7 @@ class _BackgroundHeader extends StatelessWidget {
           gradient: LinearGradient(
               begin: Alignment.topCenter,
               end: Alignment.bottomCenter,
-              colors: <Color>[Color(0xff526bf6), Color(0xff67acf2)])),
+              colors: <Color>[this.primaryColor, this.secondaryColor])),
     );
   }
 }

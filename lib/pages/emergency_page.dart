682b35fc59c6b39fec7073b07212faa35d8763c9
David Barcenas <daveepro@outlook.com>
2020-07-10 17:50:35 -0500

    feat(emergency): Add button list and install animate_do library

--- lib/pages/emergency_page.dart
+++ lib/pages/emergency_page.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:layouts/widgets/button_custom.dart';
@@ -6,19 +7,88 @@ import 'package:layouts/widgets/header.dart';
 class EmergencyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final items = <ItemBoton>[
+      new ItemBoton(FontAwesomeIcons.carCrash, 'Motor Accident',
+          Color(0xff6989F5), Color(0xff906EF5)),
+      new ItemBoton(FontAwesomeIcons.plus, 'Medical Emergency',
+          Color(0xff66A9F2), Color(0xff536CF6)),
+      new ItemBoton(FontAwesomeIcons.theaterMasks, 'Theft / Harrasement',
+          Color(0xffF2D572), Color(0xffE06AA3)),
+      new ItemBoton(FontAwesomeIcons.biking, 'Awards', Color(0xff317183),
+          Color(0xff46997D)),
+      new ItemBoton(FontAwesomeIcons.carCrash, 'Motor Accident',
+          Color(0xff6989F5), Color(0xff906EF5)),
+      new ItemBoton(FontAwesomeIcons.plus, 'Medical Emergency',
+          Color(0xff66A9F2), Color(0xff536CF6)),
+      new ItemBoton(FontAwesomeIcons.theaterMasks, 'Theft / Harrasement',
+          Color(0xffF2D572), Color(0xffE06AA3)),
+      new ItemBoton(FontAwesomeIcons.biking, 'Awards', Color(0xff317183),
+          Color(0xff46997D)),
+      new ItemBoton(FontAwesomeIcons.carCrash, 'Motor Accident',
+          Color(0xff6989F5), Color(0xff906EF5)),
+      new ItemBoton(FontAwesomeIcons.plus, 'Medical Emergency',
+          Color(0xff66A9F2), Color(0xff536CF6)),
+      new ItemBoton(FontAwesomeIcons.theaterMasks, 'Theft / Harrasement',
+          Color(0xffF2D572), Color(0xffE06AA3)),
+      new ItemBoton(FontAwesomeIcons.biking, 'Awards', Color(0xff317183),
+          Color(0xff46997D)),
+    ];
+
+    List<Widget> itemMap = items
+        .map((e) => FadeInLeft(
+              duration: Duration(milliseconds: 250),
+              child: ButtonCustom(
+                icon: e.icon,
+                text: e.text,
+                onPress: () {},
+                primaryColor: e.primaryColor,
+                seconadyColor: e.secondaryColor,
+              ),
+            ))
+        .toList();
+
     return Scaffold(
-        body: Center(
-      child: ButtonCustom(
-        text: 'Motor Accident',
-        onPress: () {
-          print('Hola desde Motor');
-        },
-      ),
+        body: Stack(
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.only(top: 240),
+          child: ListView(
+            physics: BouncingScrollPhysics(),
+            children: itemMap,
+          ),
+        ),
+        PageHeader()
+      ],
     ));
   }
 }
 
 class PageHeader extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(children: <Widget>[
+      _Header(),
+      Positioned(
+          right: -5,
+          top: 35,
+          child: RawMaterialButton(
+            onPressed: () {},
+            shape: CircleBorder(),
+            padding: EdgeInsets.all(10),
+            child: FaIcon(
+              FontAwesomeIcons.ellipsisV,
+              color: Colors.white,
+            ),
+          ))
+    ]);
+  }
+}
+
+class _Header extends StatelessWidget {
+  const _Header({
+    Key key,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return IconHeader(
@@ -27,4 +97,13 @@ class PageHeader extends StatelessWidget {
       title: 'Asistencia Médica',
     );
   }
+}
+
+class ItemBoton {
+  final IconData icon;
+  final String text;
+  final Color primaryColor;
+  final Color secondaryColor;
+
+  ItemBoton(this.icon, this.text, this.primaryColor, this.secondaryColor);
 }
